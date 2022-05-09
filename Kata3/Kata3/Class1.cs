4db35daeda522a14ebda5fcf9c087259bf587071
Craig Doyle <40270328@live.napier.ac.uk>
2022-05-09 18:42:38 +0100

    Initial creation of Kata3 project

---
+++ Kata3/Kata3/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Kata3
+{
+    public class Class1
+    {
+    }
+}
---
+++ Kata3/Kata3_Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata3_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
