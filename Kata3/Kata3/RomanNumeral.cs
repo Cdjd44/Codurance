c6ab88052022db1b450fb356569d48985f56a39c
Craig Doyle <40270328@live.napier.ac.uk>
2022-05-09 21:10:13 +0100

    refinement to convert loop (need to figure out 4 and 9)

--- Kata3/Kata3/RomanNumeral.cs
+++ Kata3/Kata3/RomanNumeral.cs
@@ -16,30 +16,14 @@ namespace Kata3
         {
             StringBuilder sb = new StringBuilder();
 
-            for (int i = 0; i < number; i++)
+            for (int i = 1; i <= number; i++)
             {
-                if (number < 4)
-                {
-                    sb.Append("I");
-                }
-
-                if (number == 4)
-                {
-                    sb.Append("I");
-                    sb.Append("V");
-                    break;
-                }
-                else if (number == 5)
-                {
-                    sb.Append("V");
-                    break;
-                }
-                else if (i == 0)
-                {
-                    sb.Append("V");
-                    i = 5;
-                }
                 sb.Append("I");
+
+                if(i % 5 == 0)
+                {
+                    sb.Replace(sb.ToString(), "V");
+                }
 
             }
 
